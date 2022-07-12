41e6a5c93b595f6b799d3c5b432e540558d3fe71
s0572842 <s0572842@htw-berlin.de>
2022-07-12 23:34:38 +0200

    added Compose Constraint

--- app/src/main/java/com/example/composetesting/layouts/constraintlayout/ConstraintLayoutComposeActivity.kt
+++ app/src/main/java/com/example/composetesting/layouts/constraintlayout/ConstraintLayoutComposeActivity.kt
@@ -4,24 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.example.composetesting.ui.theme.*
+
 
 class ConstraintLayoutComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,12 +46,7 @@ class ConstraintLayoutComposeActivity : ComponentActivity() {
                             }
                         )
                     }, content = {
-                        Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            ConstraintLayoutExample()
-                        }
+                        ConstraintLayoutExample()
                     }
                 )
             }
@@ -60,9 +56,108 @@ class ConstraintLayoutComposeActivity : ComponentActivity() {
 
 @Composable
 private fun ConstraintLayoutExample() {
-    Box(modifier = Modifier
-        .size(100.dp)
-        .background(Cyan))
+    ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val (purple, orange, blue, green, leftView, middleView, rightView) = createRefs()
+
+        Text(
+            textAlign = TextAlign.Center,
+            text = "constraint to start, top and end of parent with a horizontal bias of 30/70",
+            modifier = Modifier
+                .padding(top = 80.dp)
+                .width(150.dp)
+                .background(Purple200)
+                .padding(10.dp)
+                .constrainAs(purple) {
+                    linkTo(
+                        parent.start,
+                        parent.end,
+                        bias = 0.3F
+                    )
+                }
+        )
+
+        Text(
+            textAlign = TextAlign.Center,
+            text = "constraint via circle radius(170dp) and angle(135) to purple",
+            modifier = Modifier
+                .width(180.dp)
+                .background(Orange200)
+                .padding(10.dp)
+                .constrainAs(orange) {
+                    circular(purple, angle = 135F, distance = 170.dp)
+                }
+        )
+
+        Text(
+            textAlign = TextAlign.Center,
+            text = "Chain A, both are bi-directional connected",
+            modifier = Modifier
+                .width(120.dp)
+                .background(Green200)
+                .padding(10.dp)
+                .constrainAs(green) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                }
+        )
+
+        Text(
+            text = "Chain B, and chained with chainStyle \"Spread\"",
+            modifier = Modifier
+                .width(120.dp)
+                .background(Blue200)
+                .padding(10.dp)
+                .constrainAs(blue) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                },
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = "chained with 30 percent",
+            modifier = Modifier
+                .background(Red200)
+                .height(60.dp)
+                .padding(10.dp)
+                .constrainAs(leftView) {
+                    bottom.linkTo(parent.bottom)
+                    width = Dimension.percent(0.3F)
+                },
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = "chained with 40 percent",
+            modifier = Modifier
+                .background(Color.Gray)
+                .height(60.dp)
+                .padding(10.dp)
+                .constrainAs(middleView) {
+                    bottom.linkTo(parent.bottom)
+                    width = Dimension.percent(0.4F)
+                },
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            textAlign = TextAlign.Center,
+            text = "chained with 30 percent",
+            modifier = Modifier
+                .background(Color.Cyan)
+                .height(60.dp)
+                .padding(10.dp)
+                .constrainAs(rightView) {
+                    bottom.linkTo(parent.bottom)
+                    width = Dimension.percent(0.3F)
+                }
+        )
+
+        val chainRefOne = createHorizontalChain(green, blue, chainStyle = ChainStyle.Spread)
+        val chainRefTwo = createHorizontalChain(leftView, middleView, rightView, chainStyle = ChainStyle.Spread)
+    }
 }
 
 @Preview(showBackground = true, device = Devices.NEXUS_6, showSystemUi = true)
