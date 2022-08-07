d32a3b45b573f0cfebae5bc82801954a1f161eb9
Justus Saringer <justus.saringer@check24.de>
2022-08-07 21:49:50 +0200

    added BottomAppBar for Android View

--- app/src/main/java/com/example/composetesting/bars/bottomappbar/BottomAppBarViewActivity.kt
+++ app/src/main/java/com/example/composetesting/bars/bottomappbar/BottomAppBarViewActivity.kt
@@ -2,12 +2,44 @@ package com.example.composetesting.bars.bottomappbar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.composetesting.R
+import com.example.composetesting.databinding.BottomAppBarBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BottomAppBarViewActivity : AppCompatActivity() {
+
+    lateinit var binding: BottomAppBarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.bottom_app_bar)
+        binding = BottomAppBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "BottomAppBar"
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        setCurrentFragment(firstFragment)
+
+        binding.bottomAppBar.setOnMenuItemClickListener() {
+            when (it.itemId) {
+                R.id.first_fragment_icon -> {
+                    setCurrentFragment(firstFragment)
+                }
+                R.id.second_fragment_icon -> {
+                    setCurrentFragment(secondFragment)
+                }
+            }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            commit()
+        }
 }
