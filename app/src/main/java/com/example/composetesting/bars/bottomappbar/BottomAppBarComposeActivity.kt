285e34850040bbd2017d5e442d350709536f7256
Justus Saringer <justus.saringer@check24.de>
2022-08-07 14:55:32 +0200

    added BottomAppBar in Compose

--- app/src/main/java/com/example/composetesting/bars/bottomappbar/BottomAppBarComposeActivity.kt
+++ app/src/main/java/com/example/composetesting/bars/bottomappbar/BottomAppBarComposeActivity.kt
@@ -3,47 +3,99 @@ package com.example.composetesting.bars.bottomappbar
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Download
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.composetesting.ui.theme.ComposeTestingTheme
 
 class BottomAppBarComposeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             ComposeTestingTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = { Text(text = "BottomAppBar") },
-                            backgroundColor = MaterialTheme.colors.primary,
-                            contentColor = MaterialTheme.colors.onPrimary
-                        )
-                    },
-                    bottomBar = {
-                        BottomAppBar() {
-                            IconButton(onClick = { }) {
-                                Icon(
-                                    imageVector = Icons.Default.Search,
-                                    contentDescription = null
-                                )
-                            }
-
-                            IconButton(onClick = { }) {
-                                Icon(
-                                    imageVector = Icons.Default.Favorite,
-                                    contentDescription = null
-                                )
-                            }
-                        }
-                    }
-                ){
-
-                }
+                BottomAppBarExample()
             }
+        }
+    }
+}
+
+sealed class BottomBarScreen(
+    val title: String,
+    val icon: ImageVector
+) {
+    object FirstScreen : BottomBarScreen(
+        title = "FirstScreen",
+        icon = Icons.Default.Favorite
+    )
+
+    object SecondScreen : BottomBarScreen(
+        title = "SecondScreen",
+        icon = Icons.Default.Search
+    )
+}
+
+@Composable
+private fun BottomAppBarExample() {
+
+    val screens = listOf(
+        BottomBarScreen.FirstScreen,
+        BottomBarScreen.SecondScreen
+    )
+
+    var currentScreen by remember { mutableStateOf<BottomBarScreen>(BottomBarScreen.FirstScreen) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "BottomAppBar")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBarDetails(screens, currentScreen) { screen ->
+                currentScreen = screen
+            }
+        }
+    ) {
+        when (currentScreen) {
+            BottomBarScreen.FirstScreen -> FirstScreen()
+            BottomBarScreen.SecondScreen -> SecondScreen()
+        }
+    }
+}
+
+@Composable
+private fun BottomAppBarDetails(
+    screens: List<BottomBarScreen>,
+    currentScreen: BottomBarScreen,
+    onClick : (BottomBarScreen) -> Unit
+) {
+    BottomAppBar(cutoutShape = CircleShape) {
+        screens.forEach { screen ->
+            BottomNavigationItem(
+                label = { Text(text = screen.title) },
+                icon = {
+                    Icon(
+                        imageVector = screen.icon,
+                        contentDescription = "Navigation Icon"
+                    )
+                },
+                selected = screen == currentScreen,
+                onClick = {
+                    onClick(screen)
+                },
+                alwaysShowLabel = false
+            )
         }
     }
 }
--- app/src/main/java/com/example/composetesting/bars/bottomappbar/FirstScreen.kt
+++ app/src/main/java/com/example/composetesting/bars/bottomappbar/FirstScreen.kt
@@ -1,2 +1,34 @@
 package com.example.composetesting.bars.bottomappbar
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composetesting.ui.theme.ComposeTestingTheme
+
+@Composable
+internal fun FirstScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "First Screen",
+            Modifier.background(Color.Gray)
+        )
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4, showSystemUi = true)
+@Composable
+private fun DownloadScreenPreview() {
+    ComposeTestingTheme {
+        FirstScreen()
+    }
+}
